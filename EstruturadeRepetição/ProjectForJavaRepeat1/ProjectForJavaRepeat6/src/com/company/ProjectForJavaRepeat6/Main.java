fb1cde8da87d5cb057b429d16cd3c242cad3a38f
ClaudioGH <83303081+ClaudioGH@users.noreply.github.com>
2021-06-09 22:23:10 -0300

    Add files via upload

---
+++ EstruturadeRepetição/ProjectForJavaRepeat1/ProjectForJavaRepeat6/src/com/company/ProjectForJavaRepeat6/Main.java
@@ -0,0 +1,19 @@
+package com.company.ProjectForJavaRepeat6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+        for (int i = 1; i <= 20; i++) {
+            if (i == 20) {
+                System.out.print(i);
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
+
+
