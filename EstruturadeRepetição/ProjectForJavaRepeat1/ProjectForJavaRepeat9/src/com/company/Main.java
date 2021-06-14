a00c45e9f03b91e64194645cea03b2800c5fc5a9
ClaudioGH <83303081+ClaudioGH@users.noreply.github.com>
2021-06-14 15:30:56 -0300

    Add files via upload

---
+++ EstruturadeRepetição/ProjectForJavaRepeat1/ProjectForJavaRepeat9/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i<51; i+=2){
+            System.out.print(i + ". ");
+        }
+    }
+}
+
